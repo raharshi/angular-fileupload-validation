eb66ef13112fe427e0ef7fc49336e8e9dd5e517a
raharshi <raharshi.puli@gmail.com>
2020-03-30 17:43:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-fileupload-validation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-fileupload-validation)
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-root',
+   template: `
+	<app-upload></app-upload>
+    `
+})
+export class AppComponent { 
+} 
